47cd0f16ec7887e67b9c9b152831a7103196e32c
Remon Nashid <remon.sherin@gmail.com>
2020-01-27 10:56:17 -0800

    Updates

---
+++ precache-manifest.04f444ac6becef2f5f2358812e3ba8ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fbb80bc4800f0ed11f83369b04c4a6",
+    "url": "/tieredsto-app/index.html"
+  },
+  {
+    "revision": "20033fe0c0192cb1e38b",
+    "url": "/tieredsto-app/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "9b5a3f07560436ff9e75",
+    "url": "/tieredsto-app/static/css/main.3b9fece1.chunk.css"
+  },
+  {
+    "revision": "20033fe0c0192cb1e38b",
+    "url": "/tieredsto-app/static/js/2.f5bb1e18.chunk.js"
+  },
+  {
+    "revision": "cc6356c6752599e021852cede864624d",
+    "url": "/tieredsto-app/static/js/2.f5bb1e18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b5a3f07560436ff9e75",
+    "url": "/tieredsto-app/static/js/main.4adbde90.chunk.js"
+  },
+  {
+    "revision": "b874c60523004b84622c",
+    "url": "/tieredsto-app/static/js/runtime-main.d4b1c95a.js"
+  }
+]);
