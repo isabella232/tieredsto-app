8890f545932ded0e08a819ebfc928e843a277da1
Jeremias Diaz <monitz87@gmail.com>
2020-06-17 21:55:40 -0400

    Updates

---
+++ precache-manifest.18fadcbe1d4c829b20ee0bc2d49ca60c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12888d08933314300e24fa8e65cb1c05",
+    "url": "/tieredsto-app/index.html"
+  },
+  {
+    "revision": "53c5a52e93d1a13dfc5c",
+    "url": "/tieredsto-app/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "551152778a7549d2f611",
+    "url": "/tieredsto-app/static/css/main.3b9fece1.chunk.css"
+  },
+  {
+    "revision": "53c5a52e93d1a13dfc5c",
+    "url": "/tieredsto-app/static/js/2.426f63be.chunk.js"
+  },
+  {
+    "revision": "c2dfa6aa39574fc5019a276d9b5ac38b",
+    "url": "/tieredsto-app/static/js/2.426f63be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "551152778a7549d2f611",
+    "url": "/tieredsto-app/static/js/main.c9362759.chunk.js"
+  },
+  {
+    "revision": "b874c60523004b84622c",
+    "url": "/tieredsto-app/static/js/runtime-main.d4b1c95a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tieredsto-app/precache-manifest.04f444ac6becef2f5f2358812e3ba8ed.js"
+  "/tieredsto-app/precache-manifest.18fadcbe1d4c829b20ee0bc2d49ca60c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tieredsto-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
